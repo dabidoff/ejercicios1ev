4912c1ae9bb19488d534f19e23add9a328e54517
dabidoff <mickeymauser98@gmail.com>
2020-10-23 21:19:02 +0200

    "Ejercicio 1 de la unidad 2 resuelto"

---
+++ src/unidad2/Division.java
@@ -0,0 +1,17 @@
+package unidad2;
+
+public class Division {
+
+	public static void main(String[] args) {
+		
+		double num1 = 1234;
+		double num2 = 532;
+		double division = num1/num2;
+		
+		System.out.print("El resultado de dividir " +num1+ " entre " +num2+ " es igual a " +division);
+		
+		
+
+	}
+
+}
